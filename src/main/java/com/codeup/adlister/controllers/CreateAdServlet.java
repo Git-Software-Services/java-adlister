1847e68d6f3a4c484489e9f8e2424d7f4c6b6868
Your Name <dorian.l.wallace@gmail.com>
2019-02-13 12:09:42 -0500

    finished passwords exercises

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -2,6 +2,8 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Password;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,17 +15,21 @@ import java.io.IOException;
 @WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
-            .forward(request, response);
+        if(request.getSession().getAttribute("user") != null) {
+            request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+                    .forward(request, response);
+        }else{response.sendRedirect("/login");}
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            User user = (User) request.getSession().getAttribute("user");
         Ad ad = new Ad(
-            1, // for now we'll hardcode the user id
+                user.getId(),
             request.getParameter("title"),
             request.getParameter("description")
         );
-        DaoFactory.getAdsDao().insert(ad);
-        response.sendRedirect("/ads");
-    }
+            DaoFactory.getAdsDao().insert(ad);
+            response.sendRedirect("/ads");
+        }
+
 }
